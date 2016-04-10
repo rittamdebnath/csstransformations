0b415c90c8817a0ef565e0a8bf226ab91f9b7cc2
Rittam Debnath <rittamdebnath@gmail.com>
2016-04-10 14:08:00 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//
+// });
+
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document,
+        s = d.createElement('script');
+
+    s.src = '//csstransformations.disqus.com/embed.js';
+
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
